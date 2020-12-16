70154a4f5da7d81a4b239324cd5d93881b4e9b43
Andy DeLeon <agdl2003@gmail.com>
2020-12-16 15:21:02 -0700

    its finished

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@
 var p1Lives = 4;
 var p2Lives = 4;
 //holds a players wins
-var p1Wins;
-var p2Wins;
+var p1Wins=0;
+var p2Wins=0;
 
 //this variable holds which person turn it is x=true O=false
 var turn = true;
@@ -25,35 +25,323 @@ window.onbeforeunload = function() {
     return "You will lose your score are you sure you want to do that GAMER";
   };
 
-window.onclick = e => {
+/*window.onclick = e => {
     console.log(e.target);  // to get the element
     console.log(e.target.tagName);  // to get the element tag name alone
-}
+}*/
 //this part checks what was clicked and pust the appropriate X or O depending on whos yurn it is
 window.onclick = e => {
-    //console.log(e.target.id);
+    var those = e.target.innerHTML;
+    console.log(e.target.innerHTML);
     let that = e.target.id;
-    if(e.target.innerhtml!="X"||e.target.innerhtml!="O"){
-        if(turn==true){
+    if(those!=="X"){
+        if(those !=="O"){
+           if(turn==true){
             document.getElementById(that).innerHTML = "X";
             turn=false;
         }
         else if(turn==false){
             document.getElementById(that).innerHTML = "O";
             turn=true;
-        }
-        else{
-            //nothing
-        }
+        }  
+        
+  
     }
-    else{
-        //nothing
-    }
+    
+}
+
+ 
+}
 //this checks to for a winner
-    if(){
+    //X  win conditions for x vertical
+    setInterval(function(){ if(a1.innerHTML=="X"&&b1.innerHTML=="X"&&c1.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+    }
+    else if(a2.innerHTML=="X"&&b2.innerHTML=="X"&&c2.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
 
     }
-}
+    else if(a3.innerHTML=="X"&&b3.innerHTML=="X"&&c3.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+    //wins conditions for x horizontol
+    else if(a1.innerHTML=="X"&&a2.innerHTML=="X"&&a3.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+    else if(b1.innerHTML=="X"&&b2.innerHTML=="X"&&b3.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+    else if(c1.innerHTML=="X"&&c2.innerHTML=="X"&&c3.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+    //win conditions for x tilted /
+    else if(a1.innerHTML=="X"&&b2.innerHTML=="X"&&c3.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+    else if(a3.innerHTML=="X"&&b2.innerHTML=="X"&&c1.innerHTML=="X"){
+                document.getElementById("winner").innerHTML="Round goes to player1"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p2Lives-=1
+
+    }
+
+    //this is the same as above except for player2 instead of one
+    else if(a1.innerHTML=="O"&&b1.innerHTML=="O"&&c1.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    else if(a2.innerHTML=="O"&&b2.innerHTML=="O"&&c2.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    else if(a3.innerHTML=="O"&&b3.innerHTML=="O"&&c3.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    //wins conditions for x horizontol
+    else if(a1.innerHTML=="O"&&a2.innerHTML=="O"&&a3.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    else if(b1.innerHTML=="O"&&b2.innerHTML=="O"&&b3.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    else if(c1.innerHTML=="O"&&c2.innerHTML=="O"&&c3.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    //win conditions for x tilted /
+    else if(a1.innerHTML=="O"&&b2.innerHTML=="O"&&c3.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    else if(a3.innerHTML=="O"&&b2.innerHTML=="O"&&c1.innerHTML=="O"){
+                document.getElementById("winner").innerHTML="Round goes to player 2"
+
+        a1.innerHTML="-";
+        a2.innerHTML="-";
+        a3.innerHTML="-";
+        b1.innerHTML="-";
+        b2.innerHTML="-";
+        b3.innerHTML="-";
+        c1.innerHTML="-";
+        c2.innerHTML="-";
+        c3.innerHTML="-";
+        turn=true;
+        p1Lives-=1
+
+    }
+    
+    //
+    if(p1Lives<=0){
+        document.getElementById("winner").innerHTML="Player 2 wins"
+        p2Wins+=1;
+        p1Lives=4;
+        p2Lives=4;
+    }
+    else if(p2Lives<=0){
+        document.getElementById("winner").innerHTML="Player 1 wins"
+        p1Wins+=1;
+        p1Lives=4;
+        p2Lives=4;
+
+    }
+    document.getElementById("p1Lives").innerHTML=p1Lives;
+    document.getElementById("p2Lives").innerHTML=p2Lives;
+    document.getElementById("p1-wins").innerHTML=p1Wins;
+    document.getElementById("p2-wins").innerHTML=p2Wins; 
+    }, 1000);
+/*function winnerWinner(){
+  
+}*/
+
+ //setInterval(winnerWinner(),1000)
+
+
 
 
 function tic() {
